2ad8cbd91b08e607a45bfd271da2def57fad41cb
Moraalinvartija <100047926+Moraalinvartija@users.noreply.github.com>
2023-03-27 22:54:47 +0300

    Version 1.51
    Realized that versions should not start with number 1 cause 1.0 is meant for program that is ready.
    
    Added some elements.
    Now website shows fetched client data from API.
    Added form so that new clients can be added + function to that

--- inc/script.js
+++ inc/script.js
@@ -1,18 +1,38 @@
 fetch("http://localhost/clientapi/buildedapi/customers/read.php").then((data)=> {
     return data.json();
 }).then((objectData)=>{
-    console.log(objectData.data[0].client_fname);
-    let tab ="";
+    //console.log(objectData.data[0].client_fname);
+    let tab ="<thead><tr><th>Id</th> <th>First name</th><th>Last name</th><th>Address</th> </tr></thead>";
 
     for (let r of objectData.data) {
         tab += `<tr>
-        <th>${r.client_id}</th>
-        <th>${r.client_fname}</th>
-        <th>${r.client_lname}</th>
-        <th>${r.client_address}</th>     
+        <td>${r.client_id}</td>
+        <td>${r.client_fname}</td>
+        <td>${r.client_lname}</td>
+        <td>${r.client_address}</td>     
     </tr>`;
     //console.log(tab);
     }
     document.getElementById("customers_table").innerHTML = tab;
 })
 
+    function CreateUser() {
+      var formdata = new FormData();
+          var fname = document.getElementById('fname').value;
+          var lname = document.getElementById('lname').value;
+          var address = document.getElementById('address').value;
+          formdata.append("client_fname", fname);
+          formdata.append("client_lname", lname);
+          formdata.append("client_address", address);
+  
+          var requestOptions = {
+          method: 'POST',
+          body: formdata,
+          redirect: 'follow'
+  };
+  fetch("http://localhost/clientapi/buildedapi/customers/create.php", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  }
+  
